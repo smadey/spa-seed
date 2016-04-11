ecc5fa9cdc5f836e1fd0a3867808b19ceb596e8a
Smadey <smadey@163.com>
2016-04-11 12:30:26 +0800

    调整“scroll-in”组件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import vCube from './v-cube.vue';
 import vImg from './v-img.vue';
+import vScrollIn from './v-scroll-in.vue';
 import vSpinner from './v-spinner.vue';
 import vSticky from './v-sticky.vue';
 
 export {
   vCube,
   vImg,
+  vScrollIn,
   vSpinner,
   vSticky,
 };
