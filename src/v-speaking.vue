2760244f78a73980322a5875e87bcb018109e5b8
Smadey <smadey@163.com>
2016-04-12 18:29:31 +0800

    增加“v-speaking”组件

---
+++ src/v-speaking.vue
@@ -0,0 +1,44 @@
+<template>
+  <span v-bind:style="{width: max/2 + 'em'}">{{text}}</span>
+</template>
+
+<script>
+  export default {
+    props: {
+      max: {
+        type: Number,
+        default: 6,
+      },
+    },
+
+    data() {
+      return {
+        text: '.',
+      };
+    },
+
+    ready() {
+      this.update();
+    },
+
+    methods: {
+      update() {
+        let len = this.text.length;
+        len = len === this.max ? 1 : (len + 1);
+
+        this.text = Array(len + 1).join('.');
+
+        setTimeout(() => this.update(), 200);
+      },
+    },
+
+  };
+</script>
+
+<style scoped>
+  span {
+    display: inline-block;
+    font-weight: bolder;
+    letter-spacing: 2px;
+  }
+</style>
